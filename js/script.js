838e0f72444a7033d03d4a77e9d9d2e40acfb8f1
Larissa Hikari Ono <lari.hk.ono@gmail.com>
2023-09-13 10:05:31 -0300

    trocar imagens da galeria com js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -15,10 +16,48 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-  const elemento = document.querySelector.getElementById(parametro);
+  const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
   }
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log("ativa");
+  pergunta.setAttribute("aria-expanded", "true");
+  console.log(resposta);
+}
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas);
+console.log(perguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+galeria.forEach(eventosGaleria);
